6712d41807de068623295fb2b28427144e5045f8
gaoxuepeng <gaoxuepeng@wzdai.com>
2018-05-03 16:47:58 +0800

    引入第三方views,model,core,controller,params,migrates

--- console/config/main.php
+++ console/config/main.php
@@ -20,9 +20,48 @@ return [
      */
     'controllerNamespace' => 'console\controllers',
 
+    'controllerMap' => [
+         'migrate' => [
+              'class' => 'yii\console\controllers\MigrateController',
+//              'migrationNamespaces' => [
+//                  'app\migrations',
+//                  'app\yii2admin\migrations',
+//              ],
+              'migrationPath' => ['@app/migrations','@app/yii2admin/migrations'], // allows to disable not namespaced migration completely
+          ],
+    ],
+
     #日志会用到，$this->logFile = Yii::$app->getRuntimePath() . '/logs/app.log';
     'runtimePath' => DOCROOT . '/console/runtime',
 
     #组件
     'components' => []
 ];
+
+
+/*
+迁移
+
+yii\console\Application 初始化一些系统命令，如迁移migrate/help..
+
+
+foreach ($this->coreCommands() as $id => $command) {
+    if (!isset($this->controllerMap[$id])) {
+        $this->controllerMap[$id] = $command;  ---------------controller映射
+    }
+}
+return [
+    'asset' => 'yii\console\controllers\AssetController',
+    'cache' => 'yii\console\controllers\CacheController',
+    'fixture' => 'yii\console\controllers\FixtureController',
+    'help' => 'yii\console\controllers\HelpController',
+    'message' => 'yii\console\controllers\MessageController',
+    'migrate' => 'yii\console\controllers\MigrateController', ----------迁移
+    'serve' => 'yii\console\controllers\ServeController',
+];
+
+
+
+
+
+*/
