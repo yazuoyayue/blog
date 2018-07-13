4f7717b6d6ce7b9bbdfb480549a5286e6300d451
gaoxuepeng <gaoxuepeng@wzdai.com>
2018-07-13 10:18:23 +0800

    excel

--- enviornments/dev/config/main.php
+++ enviornments/dev/config/main.php
@@ -5,11 +5,13 @@
  * Date: 2018/4/2
  * Time: 10:27
  */
+$mysql_db = '127.0.0.1';
+$mysql_db_master = 'yazuoyayue_blog';
 
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=127.0.0.1;dbname=yazuoyayue_blog',
+            'dsn' => "mysql:host=$mysql_db;dbname=$mysql_db_master",
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8'
